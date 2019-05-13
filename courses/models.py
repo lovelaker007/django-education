2312bda7eb01c160400a144ed43040aeb87e1e9c
lovelaker007 <lovelaker7@gmail.com>
2019-05-13 16:59:18 +0800

    学生选课

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from .fields import OrderField
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 
 
 class Subject(models.Model):
@@ -24,6 +26,8 @@ class Course(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     overview = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    # 学生选课字段，学生和课程是多对多的关系
+    students = models.ManyToManyField(User, related_name='courses_joined', blank=True)
 
     class Meta:
         ordering = ['-created']
@@ -85,6 +89,8 @@ class ItemBase(models.Model):
     def __str__(self):
         return self.title
 
+    def render(self):
+        return render_to_string('courses/content/{}.html'.format(self._meta.model_name), {'item': self})
 
 class Text(ItemBase):
     content = models.TextField()
